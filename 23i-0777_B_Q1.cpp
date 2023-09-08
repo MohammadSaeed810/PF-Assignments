dac54b0ffcbbcc8697b8cc6cc69df1624ad5ae3c
Mohammad Saeed <hafizmsaeed810@gmail.com>
2023-09-08 13:11:06 +0500

    Q1 - Completed

---
+++ 23i-0777_B_Q1.cpp
@@ -0,0 +1,95 @@
+/*
+    Assignment # 1
+
+    Name    : Mohammad Saeed
+    Roll    : 23i-0777
+    Section : B
+
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout<<" ."<<setw(43)<<setfill('-')<<","<<endl
+        <<"| ."<<setw(42)<<setfill('-')<<", |"<<endl
+        <<"| |  _~_"<<setw(37)<<setfill(' ')<<"| |"<<endl
+        <<"| |   \\/  NUCES Instruments"<<setw(18)<<setfill(' ')<<"T I - 86  | |"<<endl
+        <<"| | ."<<setw(40)<<setfill('-') << ", | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | | 1 + 1"<<setw(34)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"2  | | |"<<endl
+        <<"| | | 6 * 8"<<setw(34)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"48  | | |"<<endl // 7th line
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | | _"<<setw(38)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | |"<<setw(40)<<setfill(' ')<<"| | |"<<endl
+        <<"| | `"<<setw(40)<<setfill('-') << "' | |"<<endl
+        <<"| |"<<setw(42)<<setfill(' ')<<"| |"<<endl
+        <<"| `"<<setw(42)<<setfill('-')<<"' |"<<endl
+        <<"|     M1      M2      M3      M4      M5    |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  |  F1 | |  F1 | |  F1 | |  F1 | |  F1 |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|-------.________          ________,--------|"<<endl
+        <<"|"<<setw(26)<<setfill(' ')<<"`--------'"<<setw(18)<<setfill(' ')<<"|"<<endl
+        <<"|"<<setw(44)<<setfill(' ')<<"|"<<endl
+        <<"|"<<setw(44)<<setfill(' ')<<"QUIT    MODE        _,-._        |" <<endl
+        <<"|  .-----' .-----' .-----'  /\\ \\_^_/ /\\"  <<setw(6)   <<"|"   <<endl
+        <<"|  | 2nd | | EXIT| | MORE| / /   |   \\ \\"  <<setw(5)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' |<|   O   |>|"    <<setw(5)   <<"|"   <<endl
+        <<"|  alpha   LINK x  INS     \\ \\  _|_  / /"  <<setw(5)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----'  \\/ / v \\ \\/"  <<setw(6)   <<"|"   <<endl
+        <<"|  | 2nd | | EXIT| | MORE|     -._,-"        <<setw(9)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' "                 <<setw(18)  <<"|"   <<endl
+        <<"|  SOLVER  SIMULT  POLY   CATLG-VARS "       <<setw(8)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  |GRAPH| |TABLE| | PRGM| |CUSTM| |CLEAR|  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  10x  A  SIN-1 B COS-1 C TAN-1 D pi   E"   <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  | LOG | | SIN | | COS | | TAN | |  ^  |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  ex   F  x-1  G  [    H  ]    I  CALC J"   <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  | LN  | | EE  | |  (  | |  )  | |     |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  ./~  K  MATRX L VECTR M CPLX N  MATH O"   <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  | X2  | |  7  | |  8  | |  9  | |  x  |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  /_   P  CONS Q  CONV R  STRNG S LIST T"  <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  |  ,  | |  4  | |  5  | |  6  | |  -  |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  RCL  =  BASE U  TEST V  MEM  W  STAT X"   <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  | STO>| |  1  | |  2  | |  3  | |  +  |  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  OFF     CHAR Y  :    z  ANS  _  ENTRY "    <<setw(4)   <<"|"   <<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  | ON  | |  0  | |  .  | | (-) | |ENTER|  |"<<endl
+        <<"|  .-----' .-----' .-----' .-----' .-----'  |"<<endl
+        <<"|  -------"<<setw(35)<<setfill(' ')<<"|"         <<endl
+        <<"`\\"<<setw(43)<<setfill(' ')<<"/'"               <<endl
+        <<"  `-._"<<setw(37)<<setfill(' ')<<"_.-'"          <<endl
+        <<"      `--.__"<<setw(27)<<setfill(' ')<<"__.--'"  <<endl
+        <<"            ``----.._____,,--- "
+///Last Statement is unfinished
+        
+        
+
+
+
+        ;
+  
+    return 0;
+}
